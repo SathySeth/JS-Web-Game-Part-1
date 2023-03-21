2c0fa71468669f2a477990d3ca1eaef68bedbbcf
Your Name <you@example.com>
2023-03-21 14:41:04 -0700

    changes

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+
 function newImage(url, left, bottom){
-    let element = document.createElement('img')
-    element.src = url
-    element.style.position = 'fixed'
-    element.style.left = `${left}px`
-    element.style.bottom = `${bottom}px`
-    document.body.append(element)
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
-function newItem(url,left,bottom){
-    let object = newImage(url,left,bottom)
-    object.addEventListener('dblclick', function(){
-    object.remove()
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
     })
 }
+
 newImage('assets/pine-tree.png',450,200)
 newImage('assets/green-Character.gif',100,100)
 newImage('assets/tree.png',200,300)
@@ -23,5 +27,3 @@ newImage('assets/well.png',500,424)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
-
-  
