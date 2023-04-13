a8fcf606de64a67462bd1a1417f9b0607f0af80d
Your Name <you@example.com>
2023-04-13 15:08:44 -0700

    changes

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-
- 
-function newImage() {
-    let greenCharacter = document.createElement('img')
-    greenCharacter.src = 'assets/green-character.gif'
-    greenCharacter.style.position = 'fixed'
-    greenCharacter.style.left = '100px'
-    greenCharacter.style.bottom = '100px'
-    document.body.append(greenCharacter)
-
-    let pineTree = document.createElement('img')
-    pineTree.src = 'assets/pine-tree.png'
-    pineTree.style.position = 'fixed'
-    pineTree.style.left = '450px'
-    pineTree.style.bottom = '200px'
-    document.body.append(pineTree)
+   
+function newImage(url, left, bottom) {
+    let element = document.createElement('img');
+    element.src = url
+    element.style.position = 'fixed'
+    element.style.left = left + 'px'
+    element.style.bottom = bottom + 'px'
+    document.body.append(element)
+    return element
 }
 
-newImage()
+function newItem(url, left, bottom){
+  let element = newImage(url, left, bottom)
+
+element.addEventListener('dblclick', function(){
+    element.remove()
+})
+}
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
